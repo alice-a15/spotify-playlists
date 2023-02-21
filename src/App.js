cd5da0a78e50c3629a0fb4122dea82f919bb3663
alice-a15 <alice.qian.mail@gmail.com>
2023-02-21 01:31:27 -0500

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>Spotify Playlists</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
